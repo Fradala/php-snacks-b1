dedaf33f915c9b645488f482616a31eff277e8e8
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-01-30 09:48:01 +0100

    snack

---
+++ snacks-2/index.php
@@ -0,0 +1,35 @@
+<?php
+
+class Persona {
+    private $age;
+
+    public function setAge($newAge) {
+        
+        if (!is_int($newAge)) {
+            
+            throw new InvalidArgumentException("L'età deve essere un numero intero.");
+        }
+
+      
+        $this->age = $newAge;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+}
+
+
+$persona = new Persona();
+
+try {
+    $persona->setAge(25);
+    echo "Età: " . $persona->getAge();
+
+    
+    $persona->setAge(55);
+    
+} catch (InvalidArgumentException $e) {
+    echo "Errore: " . $e->getMessage();
+}
+?>
